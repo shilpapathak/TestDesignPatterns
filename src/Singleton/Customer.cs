17e86d5b490c67a851df596cb31faa4c705f2409
shilpapathak <shilpa.pathak@comparethemarket.com>
2016-11-17 15:56:21 +0000

    Singleton pattern

---
+++ src/Singleton/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Singleton
+{
+    /// <summary>
+    /// Customer class 
+    /// </summary>
+    class Customer
+    {
+        private static Customer _customer;
+        protected Customer()
+        {
+            
+        }
+
+        public static Customer GetInstance()
+        {
+            //Lazy intialisation
+            //Non thread safe 
+            if (_customer == null)
+            {
+                _customer = new Customer();
+                Console.WriteLine("customer object created.");
+            }
+            return _customer;
+        }
+    }
+}
---
+++ src/Singleton/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Singleton
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            var goldLevelCustomers = Customer.GetInstance();
+            var silverLevelCustomers = Customer.GetInstance();
+
+            var customer1 = Customer.GetInstance();
+            var customer2 = Customer.GetInstance();
+            var customer3 = Customer.GetInstance();
+            var customer4 = Customer.GetInstance();
+            var customer5 = Customer.GetInstance();
+            var customer6 = Customer.GetInstance();
+
+            if (goldLevelCustomers == silverLevelCustomers)
+            {
+                Console.WriteLine("they both are same");
+            }
+
+
+            Console.ReadLine();
+        }
+
+    }
+}
