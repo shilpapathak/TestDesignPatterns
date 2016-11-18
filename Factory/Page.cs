d34fe01cf6df1761c04f4888c1d5784ce7ab5bdf
shilpapathak <shilpa.pathak@comparethemarket.com>
2016-11-18 15:01:26 +0000

    Factory pattern

---
+++ Factory/Page.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+
+namespace Factory
+{
+    abstract class Page
+    {
+    }
+
+    class SkillPage : Page
+    {
+        
+    }
+    class EducationPage : Page
+    {
+
+    }
+
+    class ExperiencePage : Page
+    {
+
+    }
+
+    class IntroductionPage : Page
+    {
+
+    }
+
+    class ResultPage : Page
+    {
+
+    }
+
+    class ConclusionPage : Page
+    {
+
+    }
+
+    class SummaryPage : Page
+    {
+
+    }
+
+    class BibliographyPage : Page
+    {
+
+    }
+
+    abstract class Document
+    {
+        private readonly List<Page> _pages = new List<Page>();
+
+        public Document()
+        {
+            this.CreatePages();
+        }
+
+        public List<Page> Pages
+        {
+            get { return _pages; } 
+        }
+
+        public abstract void CreatePages();
+    }
+
+    class Resume : Document
+    {
+        public override void CreatePages()
+        {
+            Pages.Add(new SkillPage());
+            Pages.Add(new ExperiencePage());
+            Pages.Add(new EducationPage());
+        }
+    }
+
+    class Report : Document
+    {
+        public override void CreatePages()
+        {
+            Pages.Add(new BibliographyPage());
+            Pages.Add(new ResultPage());
+            Pages.Add(new ConclusionPage());
+            Pages.Add(new SummaryPage());
+            Pages.Add(new ResultPage());
+        }
+    }
+}
---
+++ Factory/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    public class Program
+    {
+        public void Main(string[] args)
+        {
+            Document[] document = new Document[2];
+
+            document[0]= new Report();
+            document[1] = new Resume();
+
+            foreach (var doc in document)
+            {
+                Console.WriteLine("\n" +doc.GetType().Name);
+                Console.WriteLine("----------------------");
+
+                foreach (var page in doc.Pages)
+                {
+                    Console.WriteLine(page.GetType().Name);
+                }
+                
+            }
+            Console.Read();
+        }
+    }
+}
